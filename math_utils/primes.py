c45ef441e0473fa345ab02b9e9d5c74aff0f0d85
Joren Vermaercke <peter.vermaercke20@imperial.ac.uk>
2022-01-27 22:52:50 +0000

    First test of math_utils, primes detector

---
+++ math_utils/primes.py
@@ -0,0 +1,11 @@
+
+
+from cmath import sqrt
+from math import sqrt, floor
+
+def isprime(x):
+    for k in range(2,floor(sqrt(x))+1):
+        print(x,k,x%k)
+        if x%k==0:
+            return False
+    return True
